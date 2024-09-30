bca346df8d82f05148023cb3411cd82751eb6c80
Michal <michal.mo63@gmail.com>
2024-09-30 11:35:41 +0200

    zadanie 1.1 & 1.2

--- zadania_pt1/part1/src/Content.jsx
+++ zadania_pt1/part1/src/Content.jsx
@@ -1,21 +1,11 @@
+import Part from './Part'
+
 const Content = () => {
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
 	return (
 		<>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
+			<Part name="Fundamentals of React" exercises={10} />
+			<Part name="Using props to pass data" exercises={7} />
+			<Part name="State of a component" exercises={14} />
 		</>
 	)
 }
